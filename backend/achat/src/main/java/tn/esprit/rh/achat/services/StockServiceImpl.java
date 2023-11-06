7ae57b49b1b16d630861942264111f5e1a440166
moez.felhi063719 <moez.felhi063719@gmail.com>
2023-11-06 01:00:10 +0100

    mockito test

--- backend/achat/src/main/java/tn/esprit/rh/achat/services/StockServiceImpl.java
+++ backend/achat/src/main/java/tn/esprit/rh/achat/services/StockServiceImpl.java
@@ -37,7 +37,7 @@ public class StockServiceImpl implements IStockService {
 		// récuperer la date à l'instant t1
 		log.info("In method addStock");
 		return stockRepository.save(s);
-		
+
 	}
 
 	@Override
